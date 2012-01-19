15a0c506d6caafd222547a25de66b9d959e6e514
Thomas York <tly2@cornell.edu>
2012-01-19 13:28:16 -0500

    Now correctly handles float data with binwidth 1

--- bin/histogram.pl
+++ bin/histogram.pl
@@ -9,42 +9,56 @@ use Getopt::Std;
 # -u <upper. xmax> max x val to include in histogram. Default is to include all values.
 # -w <bin width> By default bin width is 1.
 
-use vars qw($opt_c $opt_l $opt_u $opt_w $opt_g);
+use vars qw($opt_c $opt_l $opt_u $opt_w $opt_g $opt_s);
 
 # get options
-getopts("c:l:u:w:hg");
+getopts("c:l:u:w:hgs:");
 
 print "# \'histogram.pl -c 1  -l -3.0 -u 11.0 -w 0.1  < data\'\n";
 print "# Use column 1 (0 based), lower upper limits -3.0 11.0, binwidth 0.1.\n";
 #print "$opt_c,  $opt_l, $opt_u, $opt_w \n";
-my $bigneg = -1.0e-300;
+my $bigneg = -1.0e300;
 my $bigpos = 1.0e300;
 
+my $nskip = $opt_s || 0;
 my $xcol = $opt_c; $xcol ||= 0;
 my ($col1, $col2) = ($xcol =~ /(\d+)m(\d+)/)? ($1, $2): ($xcol, $xcol);
 my $maxcol = ($col2 > $col1)? $col2: $col1;
 my $xmin = $opt_l; 
-my $xmax = $opt_u; #by default determined from data
+my $xmax = $opt_u || undef; #by default determined from data
 my $bw = $opt_w; $bw ||= 1.0;
 my %xhash = ();
 my %xmidhash = ();
 
 my $data_xmax = $bigneg;
 my $data_xmin = $bigpos;
+# print "data xmin, xmax: $data_xmin $data_xmax\n";
+my $data_all_ints = 1; 
+my $count_valid_lines = 0; # lines with number in requested col(s)
 while (<>) {
 	chomp;
 #print "$_ \n";
 	next if(/^#/);
 #	print "$_ \n";
 	my @cols = split(" ", $_);		#split on whitespace
-		next if(scalar @cols <= $maxcol);  #skip rows with not enough columns
-		my $x;
+		next if(scalar @cols <= $maxcol);  #skip rows with not enough columns	
+	my $x;
 	if($xcol =~ /(\d)+m(\d+)/){ # can subtract values in 2 columns
-		$x = $cols[$1] - $cols[$2];	
+	my $is_number1 = is_number($cols[$1]);
+my $is_number2 = is_number($cols[$2]);
+	next unless($is_number1 and $is_number2);
+	$count_valid_lines++;
+	$data_all_ints &&= ($is_number1 == 1 and $is_number2 == 1); 	
+	$x = $cols[$1] - $cols[$2];	
 	}else{
 		$x = $cols[$xcol];
 # print "x: $x\n";
+	my $is_number1 = is_number($x);
+		next unless($is_number1);
+$count_valid_lines++;
+	$data_all_ints &&= ($is_number1 == 1);
 	}
+	next if($count_valid_lines < $nskip);
 	if($opt_g){ 
 		$x = ($x > 0)? log($x)/log(10): -300; 
 		print "log_10(x): $x\n";
@@ -53,15 +67,17 @@ while (<>) {
 #print (join("; ", @cols), "\n");
 #print "x: $x \n\n";
 	if (defined $x) {
+#	print "x, dxmin, dxmax: $x  $data_xmin  $data_xmax\n";
+#exit;
 		$data_xmax = $x if($x > $data_xmax); # keep track of max and min x value so far.
-			$data_xmin = $x if($x < $data_xmin);
+		$data_xmin = $x if($x < $data_xmin);
 		$xhash{$x}++; # store value in hash
 	}
 }
-#print "data xmin: $data_xmin \n";
+
 $xmax ||= $data_xmax; 
 $xmin ||= ($data_xmin < 0)? $data_xmin: 0; # use cl arg if defined, otherwise 0 for nonneg data, min in data if neg.
-
+#print "xmin/max: $xmin $xma/$bw/x \n"; exit;
 #print "xmin, bw: ", $xmin, "  ", $bw,  "  ", $xmin % $bw, "\n";
 
 # $xmin -= $xmin % $bw;
@@ -72,13 +88,13 @@ if($xow >= 0){
 }else{
 	$xmin = $bw*(int $xow - 1);
 }
-print "xmin: $xmin \n";
+print "# xmin/max: $xmin $xmax\n";
 
 #print "xmin/xmax: $xmin $xmax \n";
 my $n_bins = int (($xmax - $xmin)/$bw) + 1;
 #print "$bw, $n_bins \n";
 my ($underflow_count, $overflow_count, $total_count) = (0, 0, 0);
-if ($bw == 1) {
+if ($bw == 1 and $data_all_ints) {
 	foreach my $x (keys %xhash){
 	my $xcount = (defined $xhash{$x})? $xhash{$x}: 0;
 		$total_count += $xcount;
@@ -88,6 +104,10 @@ if ($bw == 1) {
 	print "# underflow $underflow_count \n";
 	for (my $j=$xmin; $j<=$xmax; $j++) {
 		my $count = (defined $xhash{$j})? $xhash{$j}: 0;
+  $total_count += $count;
+                if($j < $xmin){ $underflow_count += $count; }
+                elsif($j > $xmax){ $overflow_count += $count; }
+
 		print $j, "  ", $count, "\n";
 	}
 	print "# overflow $overflow_count \n";
@@ -110,7 +130,15 @@ if ($bw == 1) {
 		my $count = (defined $xmidhash{$jj})? $xmidhash{$jj}: 0;
 #		print($xl, "  ", $midbin_x, "  ", $xu, "  ",  $count, "\n");
 #	print "$jj\n";
-		printf("%8.4g %8.4g %8.4g   %10i \n", $xl, $midbin_x, $xu, $count);
+		printf("%12.6g %12.6g %12.6g   %10i \n", $xl, $midbin_x, $xu, $count);
 	}
 }
 
+sub is_number{
+my $x = shift;
+if($x =~ /^\s*[-]?\d*[.]?\d*(e[-+]\d{1,3})?\s*$/){ # can have -, decimal pt., sci notation: -1.345e-07
+	return ($x =~ /^\s*[-]?\d+\s*$/)? 1 : 2; # 1 for integer, 2 for float
+}else{
+	return 0;
+}
+}
