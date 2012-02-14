a3667e7225e76f8732c8047b2080b2b6adde5586
Thomas York <tly2@cornell.edu>
2012-02-13 22:22:14 -0500

    remove subroutines moved to Mrbayes.pm to read in and analyze MrBayes output.

--- bin/tp_analyzer.pl
+++ bin/tp_analyzer.pl
@@ -1,50 +1,73 @@
 #!/usr/bin/perl -w
 use strict;
+
+use lib '/home/tomfy/Orthologger/lib';
 use List::Util qw ( min max sum );
+use TlyUtil qw ( order_newick );
+use Mrbayes;
 # read in MrBayes .run1.t file
 # remove branch lengths 
+# put newicks in canonical form (so exactly one newick for each topology)
 # store newicks in hash
-
-my $pattern = shift;		# e.g. fam98??.nex
-my $pattern1 = "$pattern.run?";
-
+# 
+my $alignment_nex_filename = shift;		# e.g. fam98??.nex
 my $burnin_frac = shift || 0.1;
 
-my $gen_param_hrefs = load_params($pattern);
+my $mrb_obj = Mrbayes->new({'alignment_nex_filename' =>$alignment_nex_filename});
 
+my $gen_param_hrefs = 
+#load_params($alignment_nex_filename);
+$mrb_obj->retrieve_param_samples(); # read in from *.run?.p file
 my ($gen_ntopo_hrefs, $newick_number_map, $number_newick_map) = 
-    load_topologies($pattern);
-
-my ($topo_count, $total_trees) = count_topologies($gen_ntopo_hrefs);
-
+#  load_topologies($alignment_nex_filename);	#read in from * .run?.t file
+$mrb_obj->retrieve_topology_samples();
+# $topo_count is
+my ($topo_count, $total_trees) = 
+#  count_topologies($gen_ntopo_hrefs);
+ $mrb_obj->count_topologies($gen_ntopo_hrefs);
 my $distinct_newicks = scalar keys %$topo_count;
 print "Distinct topologies: $distinct_newicks\n";
 
-my @trees = sort { sum(@{$topo_count->{$b}}) <=> sum(@{$topo_count->{$a}}) } keys %$topo_count;
+my @sorted_tree_numbers = sort { sum(@{$topo_count->{$b}}) <=> sum(@{$topo_count->{$a}}) } keys %$topo_count; # tree numbers, sorted by occurrences (sum of runs).
 
-print "n trees (sorted): ", scalar @trees, "\n";
-my  %number_rank_map = ();
-my %rank_number_map = ();
+my $number_id_map = 
+#  load_number_id_map($alignment_nex_filename);
+  $mrb_obj->retrieve_number_id_map();
+print "n trees (sorted): ", scalar @sorted_tree_numbers, "\n";
+my  %number_rank_map = (); # number is n for nth distinct topology visited by chain
+my %rank_number_map = (); # rank is 1 for topology with highest posterior prob. etc.
 my $index = 1;
 my $total_trees_so_far = 0;
-foreach (@trees) {
-  my $treecount = sum @{$topo_count->{$_}};
-  my @treecounts = @{$topo_count->{$_}};
-#  print "treecounts: ", join(" ", @treecounts), "\n";
+my $sum_diff = 0;
+my $L1_denom = 0; 
+foreach my $tree_number (@sorted_tree_numbers) {
+  my $treecount = sum @{$topo_count->{$tree_number}};
+  my @treecounts = @{$topo_count->{$tree_number}};
+  $L1_denom += $treecount;
+  $sum_diff += max(@treecounts) - min(@treecounts);
+  if ($treecount % 2 == 1) {
+    $sum_diff--;
+    $L1_denom--;
+  }
+
+  #  print "treecounts: ", join(" ", @treecounts), "\n";
   $total_trees_so_far += $treecount;
-  my $post_prob = $treecount/$total_trees;
-$number_rank_map{$_} = $index;
-$rank_number_map{$index} = $_;
+  my $newick_with_numbers = $number_newick_map->{$tree_number};
+my $post_prob = $treecount/$total_trees;
+  $number_rank_map{$tree_number} = $index;
+  $rank_number_map{$index} = $tree_number;
   printf("%4i %4i %4i %4i %4i  %10.5g %10.5g  %4i  %s\n", 
 	 $index, @treecounts, $treecount, $total_trees_so_far, 
-	 $post_prob, $total_trees_so_far/$total_trees, $_,
-	 $number_newick_map->{$_});
+	 $post_prob, $total_trees_so_far/$total_trees, $tree_number,
+	 $newick_with_numbers);
+  my $newick_with_ids = $mrb_obj->restore_ids_to_newick($newick_with_numbers, $number_id_map);
+print $newick_with_ids, "\n";
   $index++;
 }
-
+#print "$sum_diff, $total_trees $L1_denom\n";
+printf("topology post. distrib. L1 difference between runs: %8.5f \n", 0.5*$sum_diff/$L1_denom);
 print "total tree hits: $total_trees \n";
 print "distinct newicks: $distinct_newicks \n";
-#
 
 my $j_run = 1;
 foreach my $gen_ntopo (@$gen_ntopo_hrefs) {
@@ -66,159 +89,3 @@ print "run, trees read in $j_run  $trees_read_in \n";
   $j_run++;
 }				# loop over runs
 # end of main
-
-# operates on a newick of form (3,(6,4))
-# i.e. no whitespace, no branch lengths, ids must be numbers.
-# so just parens, commas and numbers
-# puts the leaves in order, such that at each node the
-# subtree with smaller value is on left. The value of an
-# internal node is the min of the values of the two child
-# nodes, and the value of a leave is its id, which must be a number.
-sub order_newick{
-  my $newick = shift;
-  if ($newick =~ /^(\d+)$/) {	# subtree is leaf!
-    return ($1, $newick);
-  } else {			# subtree has > 1 leaf.
-    my %label_newick = ();
-    $newick =~ /^[(](.*)[)]$/;
-    my @newick_chars = split('',$1); # without surrounding ()
-    my $lmr_paren_count = 0;
-    my ($il, $ir) = (0, 0);
-    my $n_chars = scalar @newick_chars;
-    my $min_label = 10000000;
-    foreach (@newick_chars) {
-      die "$_ ", $newick_chars[$ir], " not same!\n" if($_ ne $newick_chars[$ir]);
-      if ($_ eq '(') {
-	$lmr_paren_count++;
-      }
-      if ($_ eq ')') {
-	$lmr_paren_count--;
-      }
-
-      if (($ir == $n_chars-1) or ($_ eq ',' and $lmr_paren_count == 0)) { #split
-	my $ilast = ($ir == $n_chars-1)? $ir : $ir-1;
-	my $sub_newick = join('', @newick_chars[$il..$ilast]);
-	#       print "subnewick $sub_newick\n";
-	my ($label, $ordered_subnewick) = order_newick($sub_newick);
-	$label_newick{$label} = $ordered_subnewick;
-	$min_label = min($min_label, $label);
-	$il = $ir+1; $ir = $il; # skip the ','
-      } else {
-	$ir++;
-      }
-    }				# loop over chars in @newick_chars
-    my $ordered_newick = '';
-    foreach (sort {$a <=> $b} keys %label_newick) {
-      $ordered_newick .= $label_newick{$_} . ",";
-    }
-    $ordered_newick =~ s/,$//;
-    $ordered_newick = '(' . $ordered_newick . ')';
-    return ($min_label, $ordered_newick);
-  }
-  die "shouldnt get here, in order_newick\n";
-}
-
-
-sub load_params{
-  # read data from  run?.p file
-  # store in 
-  my $pattern = shift;		# e.g. fam9877.nex
-  my $p_files = `ls $pattern.run?.p`;
-  my @p_infiles = split(" ", $p_files);
-  my $n_runs_p = scalar @p_infiles;
-
-  # the following has one elem for each run, and it is
-  # a hash ref, with generation numbers as keys, 
-  # parameter strings (logL, TL, alpha ...) as values
-  my @gen_param_hashes = ();
-  foreach (1..$n_runs_p) {
-    push @gen_param_hashes, {};
-  }
-  #my $p_run = 1;
-  foreach my $i_run_p (1..$n_runs_p) {
-    my $p_file = "$pattern.run$i_run_p.p";
-    open  my $fhp, "<$p_file";
-
-    while (my $line = <$fhp>) {
-      chomp $line;
-      next unless($line =~ /^\s*(\d+)/);
-      #  print "$line \n";
-      my @cols = split(" ", $line);
-      my $generations = shift @cols;
-      my $param_string = join("  ", @cols);
-      $gen_param_hashes[$i_run_p-1]->{$generations} = $param_string;
-    }
-    $i_run_p++;
-  }
-  return \@gen_param_hashes;
-}
-# end of reading in parameter values
-
-sub load_topologies{
-my $pattern = shift; # e.g. fam987?.nex
-
- my $t_files = `ls $pattern.run?.t`;
-  my @t_infiles = split(" ", $t_files);
-  my $n_runs = scalar @t_infiles;
-  my @gen_ntopo_hashes = ();
-  foreach (1..$n_runs) {
-    push @gen_ntopo_hashes, {};
-  }
-my %newick_number_map = ();
-my %number_newick_map = ();
-  my $topology_count = 0;
-  foreach my $i_run (1..$n_runs) {
-    my $t_infile = "$pattern.run$i_run.t";
-    open my $fh, "<$t_infile";
-
-    # read trees in, remove branch lengths, store in array
-
-    while (my $line = <$fh>) {
-      chomp $line;
-      if ($line =~ s/tree gen[.](\d+) = .{4}\s+//) {
-	my $newick = $line;
-	my $generation = $1;
-	$newick =~ s/:[0-9e\-.]*(,|[)])/$1/g; # remove branch lengths
-	#print "[$newick]\n";
-	$newick =~ s/^\s+//; 
-	$newick =~ s/;\s*//;
-	$newick = order_newick($newick);
-	#	print $newick, "\n";
-	#		exit;
-	if (!exists $newick_number_map{$newick}) {
-	  $topology_count++;
-	  $newick_number_map{$newick} = $topology_count; # 1,2,3,...
-	  $number_newick_map{$topology_count} = $newick;
-	}
-	$gen_ntopo_hashes[$i_run-1]->{$generation} = $newick_number_map{$newick};
-      }
-    } # now $gen_ntopo_hashes[$i_run] is hash ref with generations as keys, and topology numbers as values.
-  }
-return (\@gen_ntopo_hashes, \%newick_number_map, \%number_newick_map);
-}
-
-sub count_topologies{
-my $gen_ntopo_hrefs = shift;
-my %topo_count = ();
-my $total_trees = 0;
-foreach my $i_run (1..scalar @$gen_ntopo_hrefs) {
-  my $gen_ntopo = $gen_ntopo_hrefs->[$i_run-1];
-  my $trees_read_in = scalar keys %{$gen_ntopo};
-  print "Run: $i_run. Trees read in: $trees_read_in\n";
-  # store trees from array in hash, skipping burn-in
-  my $n_burnin = int($burnin_frac * $trees_read_in);
-#  print "trees read in: $trees_read_in. Post burn-in: ", $trees_read_in - $n_burnin, "\n";
-  my @sorted_generations = sort {$a <=> $b} keys %{$gen_ntopo};
-  foreach my $i_gen (@sorted_generations[$n_burnin..$trees_read_in-1]) {
-    my $topo_number = $gen_ntopo->{$i_gen};
-    if (!exists $topo_count{$topo_number}) {
-      my @zeroes = ((0) x scalar @$gen_ntopo_hrefs);
-      $topo_count{$topo_number} = \@zeroes;
-    }
-    $topo_count{$topo_number}->[$i_run-1]++;
-    $total_trees++;
-  }
-  $i_run++;
-}
-return (\%topo_count, $total_trees);
-}
