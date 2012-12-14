84d8f3f4da1be5b3423372baa0ac7e3ffd4b5b2b
Thomas Fisher-York <tly2@cornell.edu>
2012-12-14 14:21:21 -0500

    Now handles both abc (3 cols) or m8 (12 cols) input; output is abc i.e. id1 id2 e-value.

--- bin/good_matches.pl
+++ bin/good_matches.pl
@@ -4,49 +4,62 @@ use List::Util qw ( min max sum );
 
 # 
 #my $abc_file = shift;
-my $seq_id_regex = shift || die "good_matches.pl called with no arguments. must supply sequence id regular expression as argument.\n"
- . "Usage example: good_matches.pl 'Medtr5g017860[.]1' 250 < Medtr.abc > Medtr5g017860.1.matches   \n"
+my $seq_id = shift || die "good_matches.pl called with no arguments. must supply sequence id regular expression as argument.\n"
+. "Usage example: good_matches.pl 'Medtr5g017860[.]1' 250 < Medtr.abc > Medtr5g017860.1.matches   \n"
 . "Gets the best 250 matches to Medtr5g017860.1 in Medtr.abc .";
 my $max_n_to_keep = shift || 1200;
 
+$seq_id =~ s/[.]/[.]/g;  # put .s in [] to show we only want to match the . character itself.
 
+print STDERR "seq id regex to match: $seq_id \n";
 #open my $fhin, "<$abc_file" || die "Could open $abc_file for reading. \n";
 
 my %seqpair_eval = ();
 
 while (<>) {
-    my ( $id1, $id2, $eval ) = split( " ", $_ );
+	my @cols = split(" ", $_);
 
-    my $id12_key;
-    if ( $id1 =~ /$seq_id_regex/ ) {
-        $id12_key = $id1 . "\t" . $id2;
-    }
-    elsif ( $id2 =~ /$seq_id_regex/ ) {
-        $id12_key = $id2 . "\t" . $id1;
-    }
-    else {
-        next;    # match doesn't involve sequence of interest.
-    }
+	my ( $id1, $id2, $eval );
 
-    if ( exists $seqpair_eval{$id12_key} ) {
-        my $id12_eval = $seqpair_eval{$id12_key};
-        $id12_eval = min( $id12_eval, $eval );
-        $seqpair_eval{$id12_key} = $id12_eval;
-    }
-    else {
-        $seqpair_eval{$id12_key} = $eval;
-    }
-	
+	if (scalar @cols == 12){
+		($id1, $id2, $eval) = ($cols[0], $cols[1], $cols[10]);
+	} elsif(scalar @cols == 3){
+		($id1, $id2, $eval) =  ($cols[0], $cols[1], $cols[2]); 
+	}
+	else{
+		die "File read from stdin does not appear to be either m8 (12 col) or abc (3 col) format.\n"
+	}
+
+	my $id12_key;
+	if ( $id1 =~ /$seq_id/ ) {
+		$id12_key = $id1 . "\t" . $id2;
+	}
+	elsif ( $id2 =~ /$seq_id/ ) {
+		$id12_key = $id2 . "\t" . $id1;
+	}
+	else {
+		next;    # match doesn't involve sequence of interest.
+	}
+
+	if ( exists $seqpair_eval{$id12_key} ) {
+		my $id12_eval = $seqpair_eval{$id12_key};
+		$id12_eval = min( $id12_eval, $eval );
+		$seqpair_eval{$id12_key} = $id12_eval;
+	}
+	else {
+		$seqpair_eval{$id12_key} = $eval;
+	}
+
 }
 
 my @matches = sort {$seqpair_eval{$a} <=> $seqpair_eval{$b}} keys %seqpair_eval;
 
 my $n_to_show = min($max_n_to_keep, scalar @matches);
 if($n_to_show == 0){
-print STDERR "No matches found.\n";
+	print STDERR "No matches found.\n";
 }else{
-foreach (@matches[0..$n_to_show-1]){
-	printf("%-30s  %-30s  e: %g \n", split(" ", $_), $seqpair_eval{$_});
+	foreach (@matches[0..$n_to_show-1]){
+		printf("%-30s  %-30s   %g \n", split(" ", $_), $seqpair_eval{$_});
+	}
 }
-}
-	
+
