6b09dd2cabe066eac09cb457fb33527a267b0b1d
Thomas Fisher-York <tly2@cornell.edu>
2013-03-20 16:07:57 -0400

    Changes to whitespace.

--- bin/accumulator.pl
+++ bin/accumulator.pl
@@ -1,7 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 
-#my @accum = split(" ", <>);
+# take a file where each line has several numbers representing
+# e.g. numbers of events of certain kinds in some time interval,
+# and generate a corresponding file with numbers which are
+# sums up to this point.
+
+# usage:  accumulator.pl < infile
 
 my $decay_length = shift || undef;
 
@@ -10,20 +15,20 @@ my $accumulate_col_0 = shift || 0;
 
 my @accum = split(" ", <>);
 
-while(<>){
-	my @cols = split(" ", $_);
-	foreach my $i (0..scalar @cols-1){
+while (<>) {
+  my @cols = split(" ", $_);
+  foreach my $i (0..scalar @cols-1) {
 
-		if($i == 0 and !$accumulate_col_0){
-			$accum[$i] = $cols[$i]; # col 0 is likely to be the number of steps or some such - don't want to accumulate
-		}else{
-	if(defined $decay_length){
-		$accum[$i] = (1.0 - 1.0/$decay_length) * $accum[$i] + $cols[$i]/$decay_length; 
-	}else{
-		$accum[$i] += $cols[$i];
-	}
- }
-}
-print join(" ", @accum), "\n";
+    if ($i == 0 and !$accumulate_col_0) {
+      $accum[$i] = $cols[$i]; # col 0 is likely to be the number of steps or some such - don't want to accumulate
+    } else {
+      if (defined $decay_length) {
+	$accum[$i] = (1.0 - 1.0/$decay_length) * $accum[$i] + $cols[$i]/$decay_length; 
+      } else {
+	$accum[$i] += $cols[$i];
+      }
+    }
+  }
+  print join(" ", @accum), "\n";
 }
 
