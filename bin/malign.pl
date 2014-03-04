1750b417afb9deb05493b87eb4153738c53d799e
Thomas Fisher-York <tly2@cornell.edu>
2014-03-04 15:15:37 -0500

    add option of specifying output file on command line.

--- bin/malign.pl
+++ bin/malign.pl
@@ -15,12 +15,11 @@ use Getopt::Long;
 # MKELDLDETTCHPNEVVVQKCMTSKKSRRKGRKTQVPWDERCKNVEYSPKNKRRTKKGMWNVSKNNDDVEFNALHKGKEKMIQTNVESNSSSFSEDEELRLALQKIVDLSISTKIWGRHLKVFARRN*
 # >Medtr1g026650.1 
 # MPGNNLMALLMMIFISSSTMSCIWPFISNVDLLSSSVISLIKQINKHLIVESICGGSGPIGGEVSLKPRNPQVPWDERCKNAENLVIKTKEEPKVSDIESNLVGGIVISEPRKIEIEGMWNVTNNNDQVESNFRHKGKEEMIQTNEESDSSFLHSLLKMRS*
-# >Medtr3g453170.1 
+# >Medtr3g453170.1
 # MKELDLDETIYHPNEVVVQKCMTSKKSRRKGRKTQVAWDERCKNAESSPKNKRRTRKGMWNVSKNNDDDEFNALHKGKEKMIQTNVESNSSSFSEDEELRLALQNSLIFQYPLKFGEDTSKSLLEGTNSRHQFNIEEGIKENSFEYFHIPNNIRINTLNL*
 #
 # Id Medtr...
-                                         
-
+my $start_time_seconds = time();
 my $input_filename;
 my $align_program = 'muscle';
 my $quality = 'best';
@@ -30,28 +29,32 @@ my $maxiters;
 
 # Process long cl options
 GetOptions(
-    'input_filename=s'      => \$input_filename,
-    'align_program=s'      => \$align_program,  # 'muscle' or 'mafft'
-    'quality=s' => \$quality, # 'best' or 'quick'
+	   'input_filename=s'      => \$input_filename,
+	   'align_program=s'      => \$align_program, # 'muscle' or 'mafft'
+	   'quality=s' => \$quality, # 'best' or 'quick'
 	   'output=s' => \$output_file # default is to write output to STDOUT
-);
-if(defined $output_file){
+	  );
+if (defined $output_file) {
   close $fh_out;
   open $fh_out, ">", "$output_file" or die "$!";
 }
 # my $n_taxa = shift || 21;
 # my $min_taxa = 4;
 #print STDERR "[$align_program]  [$quality]\n";
-if( !defined $align_program ){ $align_program = 'muscle'; }
-if( !defined $quality ){ $quality = 'best'; }
-print STDERR "[$align_program]  [$quality]\n"; #exit;
+if ( !defined $align_program ) {
+  $align_program = 'muscle';
+}
+if ( !defined $quality ) {
+  $quality = 'best';
+}
+print STDERR "[$align_program] [$input_filename] [$quality]\n"; #exit;
 
 my $state = 'idline';		# other possible values: fasta
 my ($qid, $fam_size, $taxa, $idline, $fasta, $do);
 open my $fh_in, "<", "$input_filename";
 my $tmp_filename = $input_filename . "_tmp";
 my $stderr_filename = $input_filename . ".stderr";
-
+my $count_alignments_done = 0;
 
 while (<$fh_in>) {
   if ($state eq 'idline') {
@@ -71,63 +74,66 @@ while (<$fh_in>) {
     $state = 'fasta';
     $fasta = '';
   } elsif ($state eq 'fasta') {
-    if(/^>/){
+    if (/^>/) {
       $fasta .= "\n" . $_;
-    }else{
-    my $fasta_line = $_;
-    chomp $fasta_line;
-    $fasta .= $fasta_line;
-#    print "fasta: $fasta \n";
-#exit;
-  }
-    if (/^\s*$/) { # only whitespace
+    } else {
+      my $fasta_line = $_;
+      chomp $fasta_line;
+      $fasta .= $fasta_line;
+      #    print "fasta: $fasta \n";
+      #exit;
+    }
+    if (/^\s*$/) {		# only whitespace
       chomp $idline;
       print $fh_out "$idline  $do \n";
-      if($do){
-      open my $fh_temp, ">", "$tmp_filename";
-      $fasta =~ s/^\n//; # not global - just initial one if present.
-      print $fh_temp "$fasta \n";
-      close $fh_temp;
+      if ($do) {
+	open my $fh_temp, ">", "$tmp_filename";
+	$fasta =~ s/^\n//; # not global - just initial one if present.
+	print $fh_temp "$fasta \n";
+	close $fh_temp;
 
 	my $alignment_cl;
-	if($quality ne 'best'  and  $quality  ne  'quick'){
+	if ($quality ne 'best'  and  $quality  ne  'quick') {
 	  warn "Parameter quality set to $quality. Not implemented. Using default ('best') quality.";
 	}
-      if($align_program eq 'muscle'){
-	if($quality eq 'best'){
-	  $maxiters = 1000;
-	}else{  # ($quality eq 'quick'){
-	  $maxiters = 2;
+	if ($align_program eq 'muscle') {
+	  if ($quality eq 'best') {
+	    $maxiters = 1000;
+	  } else {		# ($quality eq 'quick'){
+	    $maxiters = 2;
+	  }
+	  $alignment_cl = "muscle -in $tmp_filename -maxiters $maxiters 2> $stderr_filename";
+	} elsif ($align_program eq 'mafft') {
+	  if ($quality eq 'best') {
+	    $alignment_cl = "mafft --maxiterate 1000 --localpair  --inputorder $tmp_filename 2> $stderr_filename"; # L-INS-i (best mafft)
+	  } else {		# ($quality eq 'quick'){
+	    $alignment_cl = "mafft --retree 2 --inputorder $tmp_filename 2> $stderr_filename"; # mafft quick
+	  }
+	} else {
+	  warn "Parameter align_program set to $align_program. Not implemented. Using default: 'muscle'."
 	}
-	$alignment_cl = "muscle -in $tmp_filename -maxiters $maxiters 2> $stderr_filename";
-      }elsif($align_program eq 'mafft'){
-	if($quality eq 'best'){
-	  $alignment_cl = "mafft --maxiterate 1000 --localpair  --inputorder $tmp_filename 2> $stderr_filename"; # L-INS-i (best mafft)
-	}else{  # ($quality eq 'quick'){
-	  $alignment_cl = "mafft --retree 2 --inputorder $tmp_filename 2> $stderr_filename"; # mafft quick
-	}
-      }else{
-	warn "Parameter align_program set to $align_program. Not implemented. Using default: 'muscle'."
+	my $fasta_alignment = `$alignment_cl`; 
+	$count_alignments_done++;
+	print $fh_out "$fasta_alignment \n";
+      } else {
+	print $fh_out "\n";
       }
-      my $fasta_alignment = `$alignment_cl`; 
-      print $fh_out "$fasta_alignment \n";
-    }else{
-      print $fh_out "\n";
-    }
       $state = 'idline';
     }
   }
 }
+my $end_time_seconds = time();
+print STDERR "Alignments done: $count_alignments_done in ", $end_time_seconds - $start_time_seconds, " seconds\n";
 
 
 sub check_taxon_list{ # check list of taxa to see how many monocot species are
-# present, and whether selaginella is present.
-my $taxa = shift;
+  # present, and whether selaginella is present.
+  my $taxa = shift;
 
-my $monocot_count = 0;
-$monocot_count += 1 if($taxa =~ /Oryza_sativa/);
-$monocot_count += 1 if($taxa =~ /Sorghum_bicolor/);
-$monocot_count += 1 if($taxa =~ /Brachypodium_distachyon/);
-$monocot_count += 1 if($taxa =~ /Zea_mays/);
-return ($monocot_count, $taxa =~ (/Selaginella/)? 1 : 0);
+  my $monocot_count = 0;
+  $monocot_count += 1 if($taxa =~ /Oryza_sativa/);
+  $monocot_count += 1 if($taxa =~ /Sorghum_bicolor/);
+  $monocot_count += 1 if($taxa =~ /Brachypodium_distachyon/);
+  $monocot_count += 1 if($taxa =~ /Zea_mays/);
+  return ($monocot_count, $taxa =~ (/Selaginella/)? 1 : 0);
 }
