422b32c89a393efec590089e20f9c5cfb6385ec4
Thomas Fisher-York <tly2@cornell.edu>
2014-11-20 12:53:42 -0500

    handle any number of sets, output number of pts which fall in exactly 1 region, 2 regions, etc.

--- bin/multivenn.pl
+++ bin/multivenn.pl
@@ -1,7 +1,10 @@
 #!/usr/bin/perl -w
+use List::Util qw(min max sum);
 use strict;
 
 my @files = @ARGV;
+
+my $n_sets = scalar @files; 
 
 # arguments are 2 or more filenames, of files with ids in first col.
 # outputs list of ids, with string (e.g. 1 0 1 1 ) indicating which files it was present in,
@@ -23,12 +26,12 @@ for my $the_file (@files){ # for each input file, store id:count key:value pairs
 	}
 	push @file_id_hashes, $file_id_hash;
 }
-my %vennregion_count = (
-			'0 0 0 0 ' => 0, '0 0 0 1 ' => 0, '0 0 1 0 ' => 0, '0 0 1 1 ' => 0,
-			'0 1 0 0 ' => 0, '0 1 0 1 ' => 0, '0 1 1 0 ' => 0, '0 1 1 1 ' => 0,
-			'1 0 0 0 ' => 0, '1 0 0 1 ' => 0, '1 0 1 0 ' => 0, '1 0 1 1 ' => 0,
-			'1 1 0 0 ' => 0, '1 1 0 1 ' => 0, '1 1 1 0 ' => 0, '1 1 1 1 ' => 0,
-);
+my %vennregion_count = ();
+		# 	'0 0 0 0 ' => 0, '0 0 0 1 ' => 0, '0 0 1 0 ' => 0, '0 0 1 1 ' => 0,
+# 			'0 1 0 0 ' => 0, '0 1 0 1 ' => 0, '0 1 1 0 ' => 0, '0 1 1 1 ' => 0,
+# 			'1 0 0 0 ' => 0, '1 0 0 1 ' => 0, '1 0 1 0 ' => 0, '1 0 1 1 ' => 0,
+# 			'1 1 0 0 ' => 0, '1 1 0 1 ' => 0, '1 1 1 0 ' => 0, '1 1 1 1 ' => 0,
+# );
 
 for my $id (keys %id_count){
 	my $count = $id_count{$id};
@@ -40,7 +43,29 @@ for my $id (keys %id_count){
 	print "$id  $id_category_string \n";
 }
 
+my @nregion_histogram = ((0) x $n_sets);
 my @skeys = sort keys %vennregion_count;
-for (@skeys){
-print "$_  ", $vennregion_count{$_}, "\n";
+for (@skeys) {
+
+ my $count = $vennregion_count{$_};
+  $_ =~ s/\s+$//;
+  my $n_region = sum(split(" ", $_));
+  $nregion_histogram[$n_region-1] += $count;
+  print "$n_region   $_   $count \n";
+}
+
+for (1..$n_sets) {
+  print $_, "  ", $nregion_histogram[$_ - 1], "\n";
+}
+
+print "union size: ", sum(@nregion_histogram), "\n";
+
+
+for my $i (0..$n_sets-1){
+  for my $j ($i+1 .. $n_sets-1){
+my $x = $files[$i] . "  " . $files[$j];
+    my $idvennout = `idvenn.pl $x`;
+chomp $idvennout;
+print "$i  $j   $idvennout \n";
+}
 }
