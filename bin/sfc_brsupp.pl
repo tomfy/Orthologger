ac065c7ff0e83109e467759024c8a9d7777e247e
Thomas Fisher-York <tly2@cornell.edu>
2015-07-11 22:18:30 -0400

    version for 23 dicots 22 dicots monocots nonangios nonAM

--- bin/sfc_brsupp.pl
+++ bin/sfc_brsupp.pl
@@ -27,7 +27,7 @@ print "# MinN_min_support, DinN_min_support, DinM_min_support, DinNA_min_support
 }
 # my $require_no_amborella_in_monocots = shift || 0;
 
-my ($AMpos_offset, $monocots_offset, $basals_offset, $non_angiosperms_offset, $AMneg_offset) = 
+my ($AMpos1023_offset, $AMpos_offset, $monocots_offset, $non_angiosperms_offset, $AMneg_offset) = 
   (0, 5, 10, 15, 20);
 
 while (<>) {
@@ -40,7 +40,7 @@ while (<>) {
   if (/^Medtr/) {
     my $monocots_node_height = $cols[$monocots_offset];
     my $AMposdicots_node_height = $cols[$AMpos_offset];
-    my $basals_node_height = $cols[$basals_offset];
+ #   my $basals_node_height = $cols[$basals_offset];
     my $nonangiosperms_node_height = $cols[$non_angiosperms_offset];
     my $AMneg_node_height = $cols[$AMneg_offset];
   
@@ -57,21 +57,27 @@ while (<>) {
     my $monocots_eps_prod = $cols[$monocots_offset+1];
     my $AMposdicots_eps_prod = $cols[$AMpos_offset+1];
     my $nonangiosperms_eps_prod = $cols[$non_angiosperms_offset+1];
-    my $basals_eps_prod = $cols[$basals_offset+1];
+ #   my $basals_eps_prod = $cols[$basals_offset+1];
     my $AMneg_eps_prod = $cols[$AMneg_offset+1];
 
     my $Pdicots_in_Monocots_support = a_in_b_support($AMposdicots_eps_prod, $monocots_eps_prod);
-    my $Monocots_in_Basals_support =  a_in_b_support($monocots_eps_prod, $basals_eps_prod);
+ #   my $Monocots_in_Basals_support =  a_in_b_support($monocots_eps_prod, $basals_eps_prod);
     my $Monocots_in_Nonangiosperms_support =  a_in_b_support($monocots_eps_prod, $nonangiosperms_eps_prod);
 my $Pdicots_in_Nonangiosperms_support = a_in_b_support($AMposdicots_eps_prod, $nonangiosperms_eps_prod);
     my $Monocots_in_Negatives_support = a_in_b_support($monocots_eps_prod, $AMneg_eps_prod);
     my $Pdicots_in_Negatives_support = a_in_b_support($AMposdicots_eps_prod, $AMneg_eps_prod);
-
+#print "strict nesting? $strict_nesting \n";
+# printf("%6.4f %6.4f %6.4f %6.4f \n",
+#	     $Pdicots_in_Monocots_support, $Monocots_in_Nonangiosperms_support, 
+#	     $Pdicots_in_Nonangiosperms_support, $Monocots_in_Negatives_support);
+#print "node heights:  $monocots_node_height  $AMneg_node_height \n";
 #max( ($AMneg_eps_prod > 0)? 1 - $AMneg_eps_prod/$monocots_eps_prod : 1, -1);
 
     chomp;
 
-    my $min_supp1 = min($Pdicots_in_Monocots_support, $Monocots_in_Basals_support, $Monocots_in_Negatives_support);
+    my $min_supp1 = min($Pdicots_in_Monocots_support, 
+# $Monocots_in_Basals_support, 
+$Monocots_in_Negatives_support);
     $min_supp1 = max($min_supp1, -1.0);
  #   my $min_supp2 = min($Pdicots_in_Monocots_support, $Monocots_in_Nonangiosperms_support, $Monocots_in_Negatives_support);
  my $min_supp2 = min($Pdicots_in_Monocots_support, $Pdicots_in_Nonangiosperms_support, $Monocots_in_Negatives_support);
@@ -94,6 +100,10 @@ my $Pdicots_in_Nonangiosperms_support = a_in_b_support($AMposdicots_eps_prod, $n
       printf("%6.4f %6.4f %6.4f %6.4f  %6.4f %6.4f\n", 
 	     $Pdicots_in_Monocots_support, $Monocots_in_Nonangiosperms_support, 
 	     $Pdicots_in_Nonangiosperms_support, $Monocots_in_Negatives_support, $min_supp1, $min_supp2);
+# print "[", ($AMneg_node_height < 0 or $AMneg_node_height > $monocots_node_height), "]", "\n";
+#      print "$AMneg_node_height  $monocots_node_height \n";
+# print "[", ($nonangiosperms_node_height < 0 or $nonangiosperms_node_height > $monocots_node_height), "]", "\n";
+# print "[", ($nonangiosperms_node_height < 0 or $nonangiosperms_node_height > $AMposdicots_node_height)
     }
   
   }
@@ -102,10 +112,10 @@ my $Pdicots_in_Nonangiosperms_support = a_in_b_support($AMposdicots_eps_prod, $n
 sub a_in_b_support{
   my $a_eps_prod = shift;
   my $b_eps_prod = shift;
-  if ($a_eps_prod == -1) {
+  if ($a_eps_prod == -1) { # a absent
     return 0;
   } else {
-    if ($b_eps_prod == -1) {
+    if ($b_eps_prod == -1) { # b absent
       return 1;
     } else {
       return a_in_b_support_inner($a_eps_prod, $b_eps_prod);
@@ -117,6 +127,7 @@ sub a_in_b_support_inner{	    # returns value indicating support for
   # clade a being nested inside of clade b, 
   # based on, e.g. local support values (such as from FastTree) for branches 
   # between roots of a and b
+  # returns a value 0<x<1 describing support for possible nestings:
   # 0 < x < 0.5 : b in a preferred,
   # x = 0.5 : indifferent,
   # 0.5 < x <= 1 : a in b preferred.
