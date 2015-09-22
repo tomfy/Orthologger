43394a8ecfd2c592cb7c3b7269adbee8787ebe79
Thomas Fisher-York <tly2@cornell.edu>
2015-09-22 11:25:57 -0400

    add 23dicots(w lupine) column, and 9monocots (w duckweek) column.

--- bin/fastas_species_content.pl
+++ bin/fastas_species_content.pl
@@ -33,7 +33,53 @@ my $taxon_groups = {
                                        'Citrus_sinensis' => 1,
                                       },
 
-                    '12_AM_monocots' => { # These are the monocots in the 50-species analysis Sept. 2014
+  '23_dicots' => {
+                                       'Aquilegia_coerulea' => 1, # columbine
+
+                                       'Solanum_lycopersicum' => 1, # tomato
+                                       'Solanum_tuberosum'    => 1, # potato
+                                       'Mimulus_guttatus' => 1, # monkeyflower
+                                       'Fraxinus_excelsior' => 1, # Ash
+                                       'Sesamum_indicum' => 1,
+
+                                       'Vitis_vinifera'       => 1, # grape
+
+                                       'Glycine_max'          => 1, # soy
+                                       'Phaseolus_vulgaris' => 1,
+                                         'Lupinus_angustifolius' => 1,
+                                       'Lotus_japonicus' => 1,
+                                       'Medicago_truncatula' => 1,
+
+                                       'Populus_trichocarpa'  => 1, # poplar
+                                       'Ricinus_communis'     => 1, # castor
+                                       'Cucumis_sativus'      => 1, # cucumber
+                                       'Manihot_esculenta' => 1,
+                                       'Salix_purpurea' => 1,
+
+                                       'Theobroma_cacao' => 1,
+                                       'Carica_papaya' => 1,
+                                       'Eucalyptus_grandis' => 1,
+                                       'Gossypium_raimondii' => 1,
+                                       'Citrus_clementina' => 1,
+                                       'Citrus_sinensis' => 1,
+                                      },
+                    '12_AM_monocots' => { # These are the 8 AM monocots in the 50-species analysis Sept. 2014
+                                         # + a few more
+                                         'Musa_acuminata' => 1, # banana
+                                         'Phoenix_dactylifera'     => 1, # date palm
+                                         'Elaeis_guineensis' => 1,
+
+                                         'Phyllostachys_heterocycla' => 1, # bamboo, AM ??
+                                         'Zea_mays'                => 1, # maize
+                                         'Brachypodium_distachyon' => 1,
+                                         'Sorghum_bicolor'         => 1,
+                                         'Oryza_sativa'            => 1, # rice
+                                         'Panicum_virgatum' => 1, # switchgrass
+                                         'Panicum_hallii' => 1,
+                                         'Triticum_urartu' => 1,
+                                         'Hordeum_vulgare' => 1,
+                                        },
+  '14_monocots' => { # These are the monocots in the 50-species analysis Sept. 2014
                                          # 8 AM + nonAM Spirodela
                                          'Musa_acuminata' => 1, # banana
                                          'Phoenix_dactylifera'     => 1, # date palm
@@ -48,6 +94,8 @@ my $taxon_groups = {
                                          'Panicum_hallii' => 1,
                                          'Triticum_urartu' => 1,
                                          'Hordeum_vulgare' => 1,
+                    'Spirodela_polyrhiza' => 1, # one r is correct polyrrhiza is an 'orthographical variant'
+                    'Zostera_marina' => 1,
                                         }
                    };
 
@@ -58,11 +106,15 @@ while (<>) {
       my @species = split(",", $3);
       my $AM_dicot_species_count = 0;
       my $AM_monocot_species_count = 0;
+my $x23_dicot_species_count = 0;
+my $x14_monocot_species_count = 0;
       for (@species) {
          $AM_dicot_species_count++ if(exists $taxon_groups->{'22_AM_dicots'}->{$_});
          $AM_monocot_species_count++ if(exists $taxon_groups->{'12_AM_monocots'}->{$_});
+ $x23_dicot_species_count++ if(exists $taxon_groups->{'23_dicots'}->{$_});
+         $x14_monocot_species_count++ if(exists $taxon_groups->{'14_monocots'}->{$_});
       }
- print "$id   $AM_dicot_species_count  $AM_monocot_species_count \n";
+ print "$id   $AM_dicot_species_count  $AM_monocot_species_count   $x23_dicot_species_count  $x14_monocot_species_count \n";
    }
   
 }
