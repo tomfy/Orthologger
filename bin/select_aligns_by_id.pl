10e278797327639e4f5998e4505fd1d79dc22254
Thomas Fisher-York <tly2@cornell.edu>
2015-09-23 10:48:25 -0400

    Initial commit. select alignments from a set of .alfastas files  which have query in an id file.

---
+++ bin/select_aligns_by_id.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $idfile = shift;
+my $alf_filename_pattern = shift;
+open my $fhid, "<", $idfile || die "couldnt open $idfile for reading.\n";
+
+my %id_count = ();
+for(<$fhid>){
+   next if(/^\s*#/);
+   if(/^\s*(\S+)/){
+      my $id = $1;
+      $id_count{$id1}++;
+   }
+}
+
+my @files = `ls $alf_filename_pattern`;
+print join("; ", @files), "\n";
+
+for my $alf_file (@files){
+open my $fh_alf, "<", $alf_file or die "couldnt open $alf_file for reading. \n";
+#my $idpattern = shift;
+my $print = 0;
+while(<$fh_alf>){
+	if(/^Id\s+(\S+)/){
+		if(exists $id_count{$1}){
+			$print = 1;
+#			print "$_ turning on print \n";
+		}else{ # not one of desired ids, turn off printing
+                   $print = 0;
+		}
+#		$print = 0;
+	}else{
+		print $_ if($print);
+	}
+}
+}
+
