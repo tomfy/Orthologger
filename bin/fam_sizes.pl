26a273061f6d03fb3b6a0eee7a82403271443681
Thomas Fisher-York <tly2@cornell.edu>
2015-11-18 12:05:39 -0500

    x

--- bin/fam_sizes.pl
+++ bin/fam_sizes.pl
@@ -3,20 +3,17 @@ use strict;
 
 my $fam_count = 0;
 my $fam_size = -100;
-my ($idline_famsize, $old_idline_famsize) = (0, 0);
-my ($old_id, $id) = ('no_id', 'no_id');
-while(<>){
-	if(/^Id\s+(\S+).*fam_size:\s+(\d+)/){
-   $old_idline_famsize = $idline_famsize;
-   $old_id = $id;
-   $id = $1;
-   $idline_famsize = $2;
-	print "$old_id   $fam_count   $fam_size   $old_idline_famsize\n" if($fam_size != $old_idline_famsize);
-	$fam_size = 0;
-	$fam_count++;
-}elsif(/^>/){
-	$fam_size++;
-}
+my ($id, $idline_famsize) = ('no_id', 0);
+while (<>) {
+   if (/^Id\s+(\S+).*fam_size:\s+(\d+)/) {
+      print "$id   $fam_count   $fam_size   $idline_famsize\n"  if($fam_size != $idline_famsize);
+      $id = $1;
+      $idline_famsize = $2;
+      $fam_size = 0;
+      $fam_count++;
+   } elsif (/^>/) {
+      $fam_size++;
+   }
 }
 print "$id   $fam_count   $fam_size   $idline_famsize\n" if($fam_size != $idline_famsize);
 print "# $fam_count families. Done.\n";
