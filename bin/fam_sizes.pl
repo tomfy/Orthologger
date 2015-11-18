8e364eff69be955a2aa14a3c2c80d2bff103a2e1
tomfy <tly2@cornell.edu>
2015-11-18 12:14:57 -0500

    output fam sizes in cases where fam_size field of Id line does not agree with count of > lines in fam.

--- bin/fam_sizes.pl
+++ bin/fam_sizes.pl
@@ -3,13 +3,20 @@ use strict;
 
 my $fam_count = 0;
 my $fam_size = -100;
+my ($idline_famsize, $old_idline_famsize) = (0, 0);
+my ($old_id, $id) = ('no_id', 'no_id');
 while(<>){
-	if(/^Id /){
-	print "$fam_count   $fam_size \n";
+	if(/^Id\s+(\S+).*fam_size:\s+(\d+)/){
+   $old_idline_famsize = $idline_famsize;
+   $old_id = $id;
+   $id = $1;
+   $idline_famsize = $2;
+	print "$old_id   $fam_count   $fam_size   $old_idline_famsize\n" if($fam_size != $old_idline_famsize);
 	$fam_size = 0;
 	$fam_count++;
 }elsif(/^>/){
 	$fam_size++;
 }
 }
-print "$fam_count   $fam_size \n";
+print "$id   $fam_count   $fam_size   $idline_famsize\n" if($fam_size != $idline_famsize);
+print "# $fam_count families. Done.\n";
