613a9e53c75d5d76195a40939191c3e59c869eef
Thomas Fisher-York <tly2@cornell.edu>
2016-01-02 12:35:13 -0500

    (just indenting changed)

--- bin/select_aligns_by_id.pl
+++ bin/select_aligns_by_id.pl
@@ -6,9 +6,9 @@ my $alf_filename_pattern = shift;
 open my $fhid, "<", $idfile || die "couldnt open $idfile for reading.\n";
 
 my %id_count = ();
-for(<$fhid>){
+for (<$fhid>) {
    next if(/^\s*#/);
-   if(/^\s*(\S+)/){
+   if (/^\s*(\S+)/) {
       my $id = $1;
       $id_count{$id}++;
    }
@@ -17,24 +17,24 @@ for(<$fhid>){
 my @files = `ls $alf_filename_pattern`;
 print join("; ", @files), "\n";
 
-for my $alf_file (@files){
-$alf_file =~ s/\s+$//;
-open my $fh_alf, "<", $alf_file or die "couldnt open $alf_file for reading. \n";
-#my $idpattern = shift;
-my $print = 0;
-while(<$fh_alf>){
-	if(/^Id\s+(\S+)/){
-		if(exists $id_count{$1}){
-			$print = 1;
-#			print "$_ turning on print \n";
-		}else{ # not one of desired ids, turn off printing
-                   $print = 0;
-		}
-#		$print = 0;
-	}
-#else{
-		print $_ if($print);
-#	}
-}
+for my $alf_file (@files) {
+   $alf_file =~ s/\s+$//;
+   open my $fh_alf, "<", $alf_file or die "couldnt open $alf_file for reading. \n";
+   #my $idpattern = shift;
+   my $print = 0;
+   while (<$fh_alf>) {
+      if (/^Id\s+(\S+)/) {
+         if (exists $id_count{$1}) {
+            $print = 1;
+            #			print "$_ turning on print \n";
+         } else {          # not one of desired ids, turn off printing
+            $print = 0;
+         }
+         #		$print = 0;
+      }
+      #else{
+      print $_ if($print);
+      #	}
+   }
 }
 
