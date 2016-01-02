ef48f738264ff0bf1ae2f28ac0722aa15a0af2b9
Thomas Fisher-York <tly2@cornell.edu>
2016-01-02 12:30:16 -0500

    second col is interpreted as weight, output shows sum of weights over all input files for each id.

--- bin/multivenn.pl
+++ bin/multivenn.pl
@@ -1,7 +1,29 @@
 #!/usr/bin/perl -w
-use List::Util qw(min max sum);
 use strict;
+use List::Util qw(min max sum);
+use Getopt::Long;
+use File::Basename 'dirname';
+use Cwd 'abs_path';
+my ( $bindir, $libdir );
 
+BEGIN {	    # this has to go in Begin block so happens at compile time
+  $bindir =
+    dirname( abs_path(__FILE__) ) ; # the directory containing this script (i.e. Orthologger/bin )
+  $libdir = $bindir . '/../lib';
+  $libdir = abs_path($libdir);	# collapses the bin/../lib to just lib
+}
+use lib $libdir;
+use TomfyMisc qw 'run_quicktree run_fasttree run_phyml store_gg_info timestring ';
+
+my $gg_filename = undef;
+GetOptions(
+	   'gg_file=s'           => \$gg_filename, #
+	 );
+my $id_species = undef;
+if(defined $gg_filename  and  -f $gg_filename){
+$id_species = store_gg_info($gg_filename);
+print STDERR "done storing id-species association information.\n";
+}
 
 my @files;
 if (scalar @ARGV > 1) {
@@ -37,14 +59,21 @@ for my $the_file (@files){ # for each input file, store id:count key:value pairs
 	open my $fh, "<", "$the_file";
 	while(<$fh>){
 	  next if(/^\s*#/);
-		if(/^\s*(\S+)\s/){
-			$file_id_hash->{$1}++; # stores ids in the file; one hash for each file
-			$id_count{$1}++; # stores ids and counts (summed over all files).
-		}
+          next if(/^\s*$/);
+          my ($id, $idcount) = (0, 0);
+          if(/^\s*(\S+)\s+(\d+)/){
+             ($id, $idcount) = ($1, $2);
+          }elsif(/^\s*(\S+)\s/){
+             ($id, $idcount) = ($1, 1);
+          }
+      #    print STDERR "idcount: $idcount \n";
+			$file_id_hash->{$id} += $idcount; # stores ids in the file; one hash for each file
+			$id_count{$id} += $idcount; # stores ids and counts (summed over all files).
 	}
 	push @file_id_hashes, $file_id_hash;
 }
 my %vennregion_count = ();
+#my %qsp_idcount = (); # e.g.  3 2 0 1 1 1    would mean this id occurs 3 times in file 1, 2 x in file 2, etc.
 		# 	'0 0 0 0 ' => 0, '0 0 0 1 ' => 0, '0 0 1 0 ' => 0, '0 0 1 1 ' => 0,
 # 			'0 1 0 0 ' => 0, '0 1 0 1 ' => 0, '0 1 1 0 ' => 0, '0 1 1 1 ' => 0,
 # 			'1 0 0 0 ' => 0, '1 0 0 1 ' => 0, '1 0 1 0 ' => 0, '1 0 1 1 ' => 0,
@@ -54,12 +83,18 @@ my %vennregion_count = ();
 for my $id (keys %id_count){
 	my $count = $id_count{$id};
 	my $id_category_string = '';
+        my $qsp_idcount_string = ''; # e.g.  3 2 0 1 1 1    would mean this id occurs 3 times in file 1, 2 x in file 2, etc
 	for (@file_id_hashes){
+           my $idcount = (exists $_->{$id})? $_->{$id} : 0;
+        #   print STDERR "IDCOUNT: $idcount \n";
 		$id_category_string .= (exists $_->{$id})? '1 ' : '0 ';
+           $qsp_idcount_string .= "$idcount ";
 	}
 	$vennregion_count{$id_category_string}++;
-	my $n_ones = sum(split(" ", $id_category_string));
-	print "$id  $id_category_string   $n_ones\n";
+	my $n_ones = sum(split(" ", $id_category_string)); # number of files this id occurs in.
+        my $n_fams_this_id = sum(split(" ", $qsp_idcount_string)); # number of occurences of this id, summed over files
+        my $species = (defined $id_species->{$id})? $id_species->{$id} : '-';
+	print "$id   $id_category_string  $n_ones    $qsp_idcount_string  $n_fams_this_id    $species \n";
 }
 
 my @nregion_histogram = ((0) x $n_sets);
