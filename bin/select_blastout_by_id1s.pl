3dceba0ff3956ad9c8041f90058434fecda33b02
Thomas Fisher-York <tly2@cornell.edu>
2016-01-13 11:33:13 -0500

    chomp filename

--- bin/select_blastout_by_id1s.pl
+++ bin/select_blastout_by_id1s.pl
@@ -15,22 +15,23 @@ my @m8_filenames = `ls $m8_filename_pattern`;
 
 
 my %ids = ();
-while(<$fh_ids>){
-  next if(/^\s*#/);
-  if(/^\s*(\S+)/){
-    $ids{$1}++;
-  }
+while (<$fh_ids>) {
+   next if(/^\s*#/);
+   if (/^\s*(\S+)/) {
+      $ids{$1}++;
+   }
 }
 close $fh_ids;
 
-for my $m8filename (@m8_filenames){
-open my $fh_m8, "<", "$m8filename" or die "couldn't open $m8filename for reading.\n";
-my $print_this_line = 0;
-while(<$fh_m8>){
-  if(/^\s*(\S+)/){
-    $print_this_line = (exists $ids{$1})? 1 : 0;
-  }
-  print if($print_this_line);
-}
-close $fh_m8;
+for my $m8filename (@m8_filenames) {
+   chomp $m8filename;
+   open my $fh_m8, "<", "$m8filename" or die "couldn't open $m8filename for reading.\n";
+   my $print_this_line = 0;
+   while (<$fh_m8>) {
+      if (/^\s*(\S+)/) {
+         $print_this_line = (exists $ids{$1})? 1 : 0;
+      }
+      print if($print_this_line);
+   }
+   close $fh_m8;
 }
