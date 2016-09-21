450acf8297bd8f2f99e6aa7a25c7bc954596e969
Thomas Fisher-York <tly2@cornell.edu>
2016-09-21 11:45:49 -0400

    add some comments

--- bin/get_clumped_families.pl
+++ bin/get_clumped_families.pl
@@ -83,6 +83,10 @@ my $AM35 = {
                          Lotus_japonicus => '/home/tomfy/Aug2015multiquery/lotus_japonicus_query/sapu1940set/fams200_3/1940set.abc',
                          Carica_papaya => '/home/tomfy/Aug2015multiquery/papaya_query/sapu19x_1291set/fams200_3/1291set.abc',
                          Solanum_lycopersicum => '/home/tomfy/Aug2015multiquery/tomato_query/sapu1133set/fams200_3/1133set.abc',
+                         # Medicago_truncatula => '/home/tomfy/Aug2015multiquery/medicago_query/sapu2780set/fams200_3/2780set.abc',
+                         # Lotus_japonicus => '/home/tomfy/Aug2015multiquery/lotus_japonicus_query/sapu1940set/fams200_3/1940set.abc',
+                         # Carica_papaya => '/home/tomfy/Aug2015multiquery/papaya_query/sapu19x_1291set/fams200_3/1291set.abc',
+                         # Solanum_lycopersicum => '/home/tomfy/Aug2015multiquery/tomato_query/sapu1133set/fams200_3/1133set.abc',
                          Oryza_sativa => '/home/tomfy/Aug2015multiquery/rice_query/sapu1546set/fams200_3/1546set.abc',
                          Phoenix_dactylifera => '/home/tomfy/Aug2015multiquery/date_palm_query/sapu879set/fams200_3/879set.abc',
                         );
@@ -90,31 +94,37 @@ my $AM35 = {
 my $clump_qid_filename = undef;
 my $gg_filename = undef;
 my $abcfilelist = undef;
+my $use_clade_seqs = 0; 
 GetOptions(
            'qids=s' => \$clump_qid_filename,
            'abcfilelist=s' => \$abcfilelist,
 	   'gg_file=s'           => \$gg_filename, #
+           'use_clade_seqs!' => \$use_clade_seqs, # use_clade_seqs or nouse_clade_seqs
           );
-if (defined $abcfilelist) {
+if (defined $abcfilelist) { # store abc filenames in hash (key: species; value: filename
    open my $fhlist, "<", "$abcfilelist" or die "couldn't open $abcfilelist for reading. exiting.\n";
+print "reading species abc files from $abcfilelist \n";
    %species_abcfile = ();
    for (<$fhlist>) {
       if (/^\s*(\S+)\s+(\S+)\s*$/) {
-         species_abcfile{$1} = $2;
+      #   print "$1 $2 XXX \n";
+         $species_abcfile{$1} = $2;
       }
    }
 }
-my $gg_hashref = store_gg_info($gg_filename);
+my $gg_hashref = store_gg_info($gg_filename); # store seq_id:species pairs in hash
 
+
+# read connected components (output of clumper.pl) 
 open my $fhin, "<", "$clump_qid_filename" or die "couldn't open $clump_qid_filename for reading.\n";
-my %clumpidnumber_qidset = ();
-my %clumpidnumber_clamidset = ();
+my %clumpidnumber_qidset = (); # query ids lumped together (1 for each member of connected component.)
+my %clumpidnumber_clamidset = (); # sequences in am clade of at least one of the lumped-together families.
 my %qid_clumpidnumber = ();
 my $clump_id_number = 1;
-while (my $idline = <$fhin>) {
+while (my $idline = <$fhin>) { # read info about a connected component
    next if($idline =~ /^\s*#/);
    my @cols = split(" ", $idline);
-   my ($n_qids_clumped, $clump_qids_string, $clump_amcladeids_string) = @cols[3,9,11];
+   my ($n_qids_clumped, $clump_qids_string, $clump_amcladeids_string) = @cols[3,9,11]; # size (# of query ids), qids, all amclade ids, or a connected component.
    my @clump_qids = split(",", $clump_qids_string); #print join("; ", @clump_qids), "\n";
    $clump_amcladeids_string =~ s/([|])//g;
    my @clump_amcladeids = split(",", $clump_amcladeids_string); # print join("; ", @clump_amcladeids), "\n"; exit;
@@ -124,6 +134,7 @@ while (my $idline = <$fhin>) {
       if (exists $qid_clumpidnumber{$qid}) {
          warn "query id $qid in > 1 clump? \n";
       } else {
+      #   print "$clump_id_number   $qid\n";
          $qid_clumpidnumber{$qid} = $clump_id_number;
       }
 
@@ -132,19 +143,24 @@ while (my $idline = <$fhin>) {
    for my $clamid (@clump_amcladeids) {
       $clumpidnumber_clamidset{$clump_id_number}->{$clamid} = 1;
    }  
-#   print STDERR "clump number:  $clump_id_number   qids in clump: ", scalar keys %{$clumpidnumber_qidset{$clump_id_number}}, "\n";
+  print STDERR "clump number:  $clump_id_number   qids in clump: ", scalar keys %{$clumpidnumber_qidset{$clump_id_number}}, "\n";
    $clump_id_number++;
 }
 close $fhin;
 
+while(my($s,$f) = each %species_abcfile){
+   print "$s  $f  \n";
+}
 my %clumpidnumber_allidset = (); 
 while (my($sp,$abc_filename) = each %species_abcfile) {
    open my $fhabc, "<", "$abc_filename" or die "couldn't open $abc_filename for reading. \n";
    while (my $abcline = <$fhabc>) {
       my @abccols = split(" ", $abcline);
       my ($id1, $id2, $ev) = @abccols[0,1,2];
+   #   print "$id1 $id2 $ev \n";
       if (exists $qid_clumpidnumber{$id1}) { # this qid is part of one of the clumps
          my $clump_id_number = $qid_clumpidnumber{$id1};
+      #   print "$id1 is in clump  $clump_id_number \n";
          if (exists $clumpidnumber_allidset{$clump_id_number}) {
             $clumpidnumber_allidset{$clump_id_number}->{$id2}++;
          } else {
@@ -152,6 +168,18 @@ while (my($sp,$abc_filename) = each %species_abcfile) {
          }
       } else {
          # this query id is not of interest, do nothing with this line
+       #  print "$id1  is not query of any of lumped-together fams.\n";
+      }
+      if ($use_clade_seqs) {
+         for my $clumpid (keys %clumpidnumber_clamidset) {
+            if (exists $clumpidnumber_clamidset{$clumpid}->{$id1}) {
+               if (exists $clumpidnumber_allidset{$clumpid}) {
+                  $clumpidnumber_allidset{$clumpid}->{$id2}++;
+               } else {
+                  $clumpidnumber_allidset{$clumpid} = {$id2 => 1};
+               }
+            }
+         }
       }
    }
    close $fhabc;
@@ -159,6 +187,7 @@ while (my($sp,$abc_filename) = each %species_abcfile) {
 
 my @sorted_clump_numbers = sort { scalar keys %{$clumpidnumber_allidset{$b}} <=> scalar keys %{$clumpidnumber_allidset{$a}} } keys %clumpidnumber_allidset;
 print STDERR "# clump id, n_qids, n_all_ids: \n";
+print STDERR "# how many clump numbers? ", scalar @sorted_clump_numbers, "\n";
 for my $clump_id (@sorted_clump_numbers) {
    print STDERR "$clump_id  ", 
      scalar keys %{$clumpidnumber_qidset{$clump_id}}, "  ",
