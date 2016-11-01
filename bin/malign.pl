e1b99dd6833bff4c34cb5dcc75b42419164e3c54
Thomas Fisher-York <tly2@cornell.edu>
2016-11-01 11:47:10 -0400

    (just indenting)

--- bin/malign.pl
+++ bin/malign.pl
@@ -35,12 +35,10 @@ GetOptions(
 	   'output=s' => \$output_file # default is to write output to STDOUT
 	  );
 if (defined $output_file) {
-  close $fh_out;
-  open $fh_out, ">", "$output_file" or die "$!";
+   close $fh_out;
+   open $fh_out, ">", "$output_file" or die "$!";
 }
-# my $n_taxa = shift || 21;
-# my $min_taxa = 4;
-#print STDERR "[$align_program]  [$quality]\n";
+
 print STDERR "[$align_program] [$input_filename] [$quality]\n"; #exit;
 
 my $state = 'idline';		# other possible values: fasta
@@ -60,66 +58,66 @@ while (<$fh_in>) {
          undef $qid;
          warn "id line expected, but line is: $_ \n";
       }
-    $do = 1;
-    $idline = $_;
-    $state = 'fasta';
-    $fasta = '';
-  } elsif ($state eq 'fasta') {
-    if (/^>/) {
-      $fasta .= "\n" . $_;
-    } else {
-      my $fasta_line = $_;
-      chomp $fasta_line;
-      $fasta .= $fasta_line;
-    }
-    if (/^\s*$/) {		# only whitespace
-      chomp $idline;
-      print $fh_out "$idline  $do \n";
-      if ($do) {
-	open my $fh_temp, ">", "$tmp_filename";
-	$fasta =~ s/^\n//; # not global - just initial one if present.
-	print $fh_temp "$fasta \n";
-	close $fh_temp;
-
-	my $alignment_cl;
-	if ($quality ne 'best'  and  $quality  ne  'quick' and $quality ne 'medium' and $quality ne 'groovy') {
-	  warn "Parameter quality set to $quality. Not implemented. Using default ('medium') quality.";
-	}
-	if ($align_program eq 'muscle') {
-	  if ($quality eq 'best') {
-	    $maxiters = 25;
-	}elsif($quality eq 'medium'){
-	$maxiters = 4;
-}elsif($quality eq 'groovy'){
-   $maxiters = 16;
-	  } else {		# ($quality eq 'quick'){
-	    $maxiters = 2;
-	  }
-	  $alignment_cl = "muscle -in $tmp_filename -maxiters $maxiters -maxhours 0.4  2> $stderr_filename";
-	} elsif ($align_program eq 'mafft') {
-	  if ($quality eq 'best') {
-	    $alignment_cl = 
-              "mafft --maxiterate 25 --localpair  --inputorder $tmp_filename 2> $stderr_filename"; # L-INS-i (best mafft);
-         } elsif ($quality eq 'groovy'){ # better than 'medium', not as good as 'best'
-             $alignment_cl = "mafft --retree 2 --maxiterate 25 --inputorder $tmp_filename 2> $stderr_filename";
-} elsif ($quality eq 'medium'){
-$alignment_cl = "mafft --retree 2 --maxiterate 2 --inputorder $tmp_filename 2> $stderr_filename"; # "mafft --auto $tmp_filename 2> $stderr_filename";  
-} else {		# ($quality eq 'quick'){
-	    $alignment_cl = "mafft --retree 2 --inputorder $tmp_filename 2> $stderr_filename"; # mafft quick
-	  }
-	} else {
-	  warn "Parameter align_program set to $align_program. Not implemented. Using default: 'muscle'."
-	}
-	print STDERR "Alignment command line: [$alignment_cl] \n";
-	my $fasta_alignment = `$alignment_cl`; 
-	$count_alignments_done++;
-	print $fh_out "$fasta_alignment \n";
+      $do = 1;
+      $idline = $_;
+      $state = 'fasta';
+      $fasta = '';
+   } elsif ($state eq 'fasta') {
+      if (/^>/) {
+         $fasta .= "\n" . $_;
       } else {
-	print $fh_out "\n";
+         my $fasta_line = $_;
+         chomp $fasta_line;
+         $fasta .= $fasta_line;
       }
-      $state = 'idline';
-    }
-  }
+      if (/^\s*$/) {		# only whitespace
+         chomp $idline;
+         print $fh_out "$idline  $do \n";
+         if ($do) {
+            open my $fh_temp, ">", "$tmp_filename";
+            $fasta =~ s/^\n//; # not global - just initial one if present.
+            print $fh_temp "$fasta \n";
+            close $fh_temp;
+
+            my $alignment_cl;
+            if ($quality ne 'best'  and  $quality  ne  'quick' and $quality ne 'medium' and $quality ne 'groovy') {
+               warn "Parameter quality set to $quality. Not implemented. Using default ('medium') quality.";
+            }
+            if ($align_program eq 'muscle') {
+               if ($quality eq 'best') {
+                  $maxiters = 25;
+               } elsif ($quality eq 'medium') {
+                  $maxiters = 4;
+               } elsif ($quality eq 'groovy') {
+                  $maxiters = 16;
+               } else {		# ($quality eq 'quick'){
+                  $maxiters = 2;
+               }
+               $alignment_cl = "muscle -in $tmp_filename -maxiters $maxiters -maxhours 0.4  2> $stderr_filename";
+            } elsif ($align_program eq 'mafft') {
+               if ($quality eq 'best') {
+                  $alignment_cl = 
+                    "mafft --maxiterate 25 --localpair  --inputorder $tmp_filename 2> $stderr_filename"; # L-INS-i (best mafft);
+               } elsif ($quality eq 'groovy') { # better than 'medium', not as good as 'best'
+                  $alignment_cl = "mafft --retree 2 --maxiterate 25 --inputorder $tmp_filename 2> $stderr_filename";
+               } elsif ($quality eq 'medium') {
+                  $alignment_cl = "mafft --retree 2 --maxiterate 2 --inputorder $tmp_filename 2> $stderr_filename"; # "mafft --auto $tmp_filename 2> $stderr_filename";  
+               } else {		# ($quality eq 'quick'){
+                  $alignment_cl = "mafft --retree 2 --inputorder $tmp_filename 2> $stderr_filename"; # mafft quick
+               }
+            } else {
+               warn "Parameter align_program set to $align_program. Not implemented. Using default: 'muscle'."
+            }
+            print STDERR "Alignment command line: [$alignment_cl] \n";
+            my $fasta_alignment = `$alignment_cl`; 
+            $count_alignments_done++;
+            print $fh_out "$fasta_alignment \n";
+         } else {
+            print $fh_out "\n";
+         }
+         $state = 'idline';
+      }
+   }
 }
 my $end_time_seconds = time();
 print STDERR "Alignments done: $count_alignments_done in ", $end_time_seconds - $start_time_seconds, " seconds\n";
